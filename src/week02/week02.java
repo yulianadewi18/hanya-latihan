5d135194f5a3d02a58614c5cbb1aed68676fe959
yulianadewi18 <75105622+yulianadewi18@users.noreply.github.com>
2021-03-15 21:00:02 +0700

    week02

---
+++ src/week02/week02.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02;
+
+/**
+ *
+ * @author Yuliana Dewi
+ */
+public class week02 {
+   
+    public static void main(String[] args) {
+        
+    String nama = "yuliana dewi";
+    char abjad = 'a';
+    int a =6;
+    double b = 6.9;
+    
+    System.out.println (nama);
+    System.out.println (a);
+    System.out.println (b);
+    System.out.println (abjad);
+    }
+    
+}
